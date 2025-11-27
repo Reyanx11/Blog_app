74937ca43c592bcbca3d4a4c21e85d8ab15c5b76
Reyanx11 <reyankhan735@gmail.com>
2025-11-27 20:28:47 +0530

    changed files into a package structure

---
+++ blog_app/routes.py
@@ -0,0 +1,49 @@
+from flask import render_template,url_for,flash,redirect
+from blog_app.forms import RegistrationForm, LoginForm
+from blog_app import app
+from blog_app.models import User,Post
+
+
+posts = [
+    {
+        'author':'reyan khan',
+        'title': 'post 1',
+        'content': 'first blog post',
+        'posted_on':'October 12, 2025'
+    },
+    {
+        'author':'abc',
+        'title': 'post 2',
+        'content': 'Demo post',
+        'posted_on':'October 15, 2025'
+    }
+]
+
+#routes
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title ='About')
+
+@app.route("/register", methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account has been created {form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'register', form = form)
+
+@app.route("/login", methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == '123':
+            flash('successfully logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('login failed, check your email and password', 'danger')
+    return render_template('login.html', title = 'login', form = form)
