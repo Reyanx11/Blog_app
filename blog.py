d81f5e829b7abdea661ed096703fd0069cc6ce83
Reyanx11 <reyankhan735@gmail.com>
2025-11-27 18:26:48 +0530

    added sqlachemy

--- blog.py
+++ blog.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from flask import Flask,render_template,url_for,flash,redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
@@ -7,6 +9,29 @@ load_dotenv()  # loading dotenv file
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(15), unique = True, nullable = False)
+    email = db.Column(db.String(40), unique = True, nullable = False)
+    image_file = db.Column(db.String(30),nullable=False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    posts = db.relationship('Post', backref = "author", lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(30),nullable = False)
+    content = db.Column(db.Text, nullable = False)
+    date_posted = db.Column(db.DateTime , nullable = False , default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
