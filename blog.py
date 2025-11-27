e3f5adb78e13373ee33f9d1cf31863d8bf84ad1b
Reyanx11 <reyankhan735@gmail.com>
2025-11-27 13:13:05 +0530

    added login and registration route

--- blog.py
+++ blog.py
@@ -1,6 +1,12 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loading dotenv file
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 posts = [
     {
@@ -26,6 +32,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title ='About')
+
+@app.route("/register", methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account has been created {form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'register', form = form)
+
+@app.route("/login", methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == '123':
+            flash('successfully logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('login failed, check your email and password', 'danger')
+    return render_template('login.html', title = 'register', form = form)
 
 
 if __name__ == "__main__":
