2aecdd285201d7d26b92913efbc9bd41c08a791e
Reyanx11 <reyankhan735@gmail.com>
2025-11-29 14:52:03 +0530

    added login authentication

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import length, DataRequired,Email,EqualTo
+from wtforms.validators import length, DataRequired,Email,EqualTo,ValidationError
+from blog_app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), length(min = 2, max= 20)])
@@ -9,8 +10,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username (self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose different one')
+        
+    def validate_email(self,email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose different one')
+
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(),Email()])
     password = PasswordField('password', validators=[DataRequired()])
     Remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    submit = SubmitField('Login')   
--- blog_app/routes.py
+++ blog_app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template,url_for,flash,redirect
+from flask import render_template,url_for,flash,redirect,request
 from blog_app.forms import RegistrationForm, LoginForm
-from blog_app import app
+from blog_app import app,db,bcrypt
 from blog_app.models import User,Post
+from flask_login import login_user,current_user,logout_user,login_required
 
 
 posts = [
@@ -33,6 +34,10 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account has been created {form.username.data}', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title = 'register', form = form)
@@ -41,9 +46,23 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@gmail.com' and form.password.data == '123':
-            flash('successfully logged in', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email = form.email.data).first()
+        
+        if user and bcrypt.check_password_hash(user.password , form.password.data):
+            login_user (user,remember=form.Remember.data)
+            next_page = request.args.get('next')
+            return redirect('next_page')if next_page else redirect(url_for('home'))
         else:
             flash('login failed, check your email and password', 'danger')
     return render_template('login.html', title = 'login', form = form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html',title = 'Account')
