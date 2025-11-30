696ebd189487575cb8ba860b84fbabceac4d2176
Reyanx11 <reyankhan735@gmail.com>
2025-11-30 19:00:39 +0530

    added user profile updation

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import length, DataRequired,Email,EqualTo,ValidationError
 from blog_app.models import User
@@ -23,5 +25,23 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(),Email()])
     password = PasswordField('password', validators=[DataRequired()])
-    Remember = BooleanField('Remember Me')
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')   
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), length(min = 2, max= 20)])
+    email = StringField('email', validators=[DataRequired(),Email()])
+    picture = FileField('update profile picture', validators=[FileAllowed(['.jpg','.png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose different one')
+            
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose different one')
